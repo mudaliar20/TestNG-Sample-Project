a55d8b7247b4c319704d8aad5fc925b748d2c3f9
oktayuyar <oktyuyar@gmail.com>
2018-11-02 10:12:41 +0300

    TestNG parallel test sample

---
+++ src/test/java/com/oktay/testng/test/DemoA.java
@@ -0,0 +1,15 @@
+package com.oktay.testng.test;
+
+import org.testng.annotations.Test;
+
+/*
+ *  Created by oktayuyar on 2.11.2018
+ */
+public class DemoA {
+
+    @Test
+    public void demoATest(){
+        System.out.println("Demo A");
+    }
+
+}
---
+++ src/test/java/com/oktay/testng/test/DemoB.java
@@ -0,0 +1,14 @@
+package com.oktay.testng.test;
+
+import org.testng.annotations.Test;
+
+/*
+ *  Created by oktayuyar on 2.11.2018
+ */
+public class DemoB {
+
+    @Test
+    public void demoBTest(){
+        System.out.println("Demo B");
+    }
+}
